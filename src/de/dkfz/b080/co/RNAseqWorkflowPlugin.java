8df172c8b80314d946a85192409d202811b67eee
Ishaque <n.ishaque@dkfz-heidelberg.de>
2017-08-21 15:04:27 +0200

    fixed errors in qcjson file

--- src/de/dkfz/b080/co/RNAseqWorkflowPlugin.java
+++ src/de/dkfz/b080/co/RNAseqWorkflowPlugin.java
@@ -7,8 +7,8 @@ import de.dkfz.roddy.plugins.BasePlugin;
  */
 public class RNAseqWorkflowPlugin extends BasePlugin {
 
-    public static final String CURRENT_VERSION_STRING = "1.0.21";
-    public static final String CURRENT_VERSION_BUILD_DATE = "Mon Aug 21 11:42:04 CEST 2017";
+    public static final String CURRENT_VERSION_STRING = "1.0.22";
+    public static final String CURRENT_VERSION_BUILD_DATE = "Mon Aug 21 14:55:47 CEST 2017";
 
     @Override
     public String getVersionInfo() {
