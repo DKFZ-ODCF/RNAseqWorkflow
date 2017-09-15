484487a4de870869f0db68be9a613b7b0353f883
Ishaque <n.ishaque@dkfz-heidelberg.de>
2017-09-15 10:38:28 +0200

    Added variable FEATURE_COUNT_CORES to bypass segfault with too many cores

--- resources/analysisTools/rnaseqworkflow/HIPO2_rnaseq_processing.sh
+++ resources/analysisTools/rnaseqworkflow/HIPO2_rnaseq_processing.sh
@@ -184,7 +184,7 @@ if [ "$RUN_FEATURE_COUNTS" == true ]
 then
 	make_directory $COUNT_DIR 
 	cd $COUNT_DIR
-	COUNT="-t exon -g gene_id -Q 255 -T $CORES -a $GENE_MODELS -F GTF --tmpDir $SCRATCH/${SAMPLE}_${pid}_featureCounts "
+	COUNT="-t exon -g gene_id -Q 255 -T $FEATURE_COUNT_CORES -a $GENE_MODELS -F GTF --tmpDir $SCRATCH/${SAMPLE}_${pid}_featureCounts "
 	make_directory $SCRATCH/${SAMPLE}_${pid}_featureCounts
 	for S in {0..2} 
 	do
@@ -214,7 +214,7 @@ if [ "$RUN_FEATURE_COUNTS_DEXSEQ" == true ]
 then
 	make_directory $COUNT_DIR_EXON
 	cd $COUNT_DIR_EXON
-	COUNT_EXONS="-f -O -F GTF -a $GENE_MODELS_DEXSEQ -t exonic_part -Q 255 -T $CORES --tmpDir $SCRATCH/${SAMPLE}_${pid}_featureCountsExons "
+	COUNT_EXONS="-f -O -F GTF -a $GENE_MODELS_DEXSEQ -t exonic_part -Q 255 -T $FEATURE_COUNT_CORES --tmpDir $SCRATCH/${SAMPLE}_${pid}_featureCountsExons "
 	make_directory $SCRATCH/${SAMPLE}_${pid}_featureCountsExons
 	for S in {0..2}  
 	do
